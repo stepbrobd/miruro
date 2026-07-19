9add8c1288b6a64833e42847fad9ad1dbba62e9a
Yifei Sun <ysun@hey.com>
2026-07-19 12:47:37 +0200

    flake: formatter

--- flake.nix
+++ flake.nix
@@ -37,6 +37,9 @@
       devShells.default = pkgs.mkShell {
         inputsFrom = lib.attrValues self'.packages;
         packages = with pkgs; [
+          deno
+          nixpkgs-fmt
+
           go
           go-tools
           gomod2nix
@@ -46,11 +49,29 @@
 
       formatter = pkgs.writeShellScriptBin "formatter" ''
         set -eoux pipefail
-        ${lib.getExe pkgs.go} fmt ./...
-        ${lib.getExe pkgs.go} vet ./...
+        shopt -s globstar
+
+        root="$PWD"
+        while [[ ! -f "$root/.git/index" ]]; do
+          if [[ "$root" == "/" ]]; then
+            exit 1
+          fi
+          root="$(dirname "$root")"
+        done
+        pushd "$root" > /dev/null
+
+        ${lib.getExe pkgs.deno} fmt **/*.md
         ${lib.getExe pkgs.nixpkgs-fmt} .
+
+        ${lib.getExe pkgs.go} fix ./...
+        ${lib.getExe pkgs.go} fmt ./...
+        ${lib.getExe pkgs.go} mod tidy
+        ${lib.getExe pkgs.go} test -race ./...
+        ${lib.getExe pkgs.go} vet ./...
         ${lib.getExe' pkgs.go-tools "staticcheck"} ./...
         ${lib.getExe' pkgs.gomod2nix "gomod2nix"}
+
+        popd
       '';
     };
   };
